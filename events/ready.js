4bdcdaa8b0def101e7fac69d4719a6a18d265acc
Jason <Jason@smultar.com>
2023-04-06 21:27:03 -0400

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,11 @@
+import { Events } from 'discord.js';
+import Event from '../templates/Event.js';
+
+export default new Event({
+        name: Events.ClientReady,
+        once: true,
+        execute: () => {
+            console.log(`Logged into Discord successfully as ${client.user.tag}.`.green);
+        }
+    }
+);
